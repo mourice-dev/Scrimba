ea000e407aec19fae8a49ac980e17dc030330edc
Nshuti Maurice <nshutikope@gmail.com>
2025-12-02 09:56:25 +0200

    ft/ producters controller

--- controllers/productersController.js
+++ controllers/productersController.js
@@ -1,43 +1,53 @@
 /** @format */
-import { startups } from "../data/data.js";
 
-export const productersController = (req, res) => {
-  let filteredData = startups;
+export async function getGenres() {
+  console.log("genres");
+}
 
-  const { industry, country, continent, is_seeking_funding, has_mvp } =
-    req.query;
+export async function getProducts() {
+  console.log("products");
+}
 
-  if (industry) {
-    filteredData = filteredData.filter(
-      (startup) => startup.industry.toLowerCase() === industry.toLowerCase()
-    );
-  }
+// /** @format */
+// import { startups } from "../data/data.js";
 
-  if (country) {
-    filteredData = filteredData.filter(
-      (startup) => startup.country.toLowerCase() === country.toLowerCase()
-    );
-  }
+// export const productersController = (req, res) => {
+//   let filteredData = startups;
 
-  if (continent) {
-    filteredData = filteredData.filter(
-      (startup) => startup.continent.toLowerCase() === continent.toLowerCase()
-    );
-  }
+//   const { industry, country, continent, is_seeking_funding, has_mvp } =
+//     req.query;
 
-  if (is_seeking_funding) {
-    filteredData = filteredData.filter(
-      (startup) =>
-        startup.is_seeking_funding ===
-        JSON.parse(is_seeking_funding.toLowerCase())
-    );
-  }
+//   if (industry) {
+//     filteredData = filteredData.filter(
+//       (startup) => startup.industry.toLowerCase() === industry.toLowerCase()
+//     );
+//   }
 
-  if (has_mvp) {
-    filteredData = filteredData.filter(
-      (startup) => startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
-    );
-  }
+//   if (country) {
+//     filteredData = filteredData.filter(
+//       (startup) => startup.country.toLowerCase() === country.toLowerCase()
+//     );
+//   }
 
-  res.json(filteredData);
-};
+//   if (continent) {
+//     filteredData = filteredData.filter(
+//       (startup) => startup.continent.toLowerCase() === continent.toLowerCase()
+//     );
+//   }
+
+//   if (is_seeking_funding) {
+//     filteredData = filteredData.filter(
+//       (startup) =>
+//         startup.is_seeking_funding ===
+//         JSON.parse(is_seeking_funding.toLowerCase())
+//     );
+//   }
+
+//   if (has_mvp) {
+//     filteredData = filteredData.filter(
+//       (startup) => startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
+//     );
+//   }
+
+//   res.json(filteredData);
+// };
